fbc62323bfd36cd6668b0caabb755ca241ce0d4e
deepak0922 <46101039+deepak0922@users.noreply.github.com>
2020-08-15 18:57:21 +0530

    Create file

---
+++ index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resume</title>
+    <style>
+    
+    .box{
+        width: 60%;
+        height: 100%;
+        margin:0px auto;
+        padding: 5px;
+        border: solid 2px black;  
+    }
+    .box h1{
+        text-align:center;
+    }
+    p{
+        font-size:20px;
+    }
+    .border{
+        width:100%;
+        background-color: #C0C0C0;
+    }
+    ul{
+        font-size:20px;
+        margin-left:20px;
+    }
+    h2{
+        float:right;
+    }
+    </style>
+
+</head>
+<body>
+    <div class="box">
+        <br>
+        <br>
+        <h1>RESUME</h1>
+        <br>
+        <p>
+            <b>HARSHIT VERMA</b><br>
+           <?php  
+            echo "House,223/4 Savitri Enclave"
+            ?>
+            <br>
+            <?php
+            echo "Near Walia Hotel N.H-74" 
+            ?>
+            <br>
+            <?php
+            echo "Najibabad-246763"
+            ?>
+            <br>
+            <?php echo "Mob:8126929092"
+            ?>
+            <br>
+            <?php
+            echo "Email:-harshit.verma_cs18@gla.ac.in"
+            ?>
+            <br>
+        </p>
+        <hr>
+        </hr>
+        <br>
+        <div class="border">
+          <b>
+            <i><?php echo "PROFESSIONAL GOALS:-"?></i>  
+          </b>
+        </div>
+        
+        <p>
+        <?php echo "I aim to attain an engaging internship position in the field of IT Sector hope to create online campaigns to inspire the youth."
+        ?>
+        </p>
+        <div class="border">
+          <b>
+            <i> 
+                <?php 
+                     echo "WORK EXPERIENCE:-"
+                ?> 
+            </i>
+          </b>
+         
+        </div>
+        <div>
+        <h3>
+          <i>
+            <?php echo "Graphics Designer:"
+            ?>
+            </i>
+            </h3>
+            <?php    
+                echo "Tecvis-Vision For Technology"
+            ?>
+        <em> 
+        </em>
+        <p>
+            <?php
+            echo "Designs and creates Logo, Mailers  etc solutions to solve designing problems for various clients."
+            ?>
+        </p>
+        </div>
+        <br>
+        <div class="border">
+          <b>
+            <i>  EDUCATIONAL QUALIFICATION:-</i>
+          </b>
+        </div>
+        <br>
+        <ul>
+            <li><b>10<sup>th</sup></b> Passed from <b>CBSE Board.</b></li>
+            <li><b>12<sup>th</sup></b> Passed from <b>CBSE Board.</b></li>
+            <li><b>B.Tech</b> Pursuing from <b>GLA University.</b></li>
+        </ul>
+        <br>
+        <div class="border">
+          <b>
+            <i>  TECHNICAL QUALIFICATIONS:-</i>
+          </b>
+        </div>
+        <ul>
+        <li><b>Programming Languages:</b> C , JAVA , PYTHON</li>
+        <li><b>Web Development:</b> HTML , CSS , JavaScript , PHP</li>
+        <li><b>Database:</b> MySQL</li>
+        <li><b>Operating System:</b> Windows , Linux</li>
+        </ul>
+        <br>
+        <div class="border">
+          <b>
+            <i>  CERTIFICATION:-</i>
+          </b>
+        </div>
+        <ul>
+        <li>
+        <b><i>Communication Skills</i></b> -TCS</li>
+        <li><b><i>Full Stack in Python with Flask</i></b> -IIEC Rise</li>
+        <li><b><i>A-Z Machine Learning</i></b> -Udemy</li>
+        <li><b><i>Fundamentals of Digital Marketing</i></b> -Accenture</li>
+        </ul>
+        <hr>
+        <p>
+        <h3>
+        <?php
+        echo "I  do hereby declare that the above information is true to the best of my knowledge and I am solely responsible for its authenticity."
+        ?>
+        </h3>
+        </p>
+        <p>
+            DATE.........<br>
+            PLACE........<br>
+        </p>
+        <h2>
+        <?php
+        echo "Harshit Verma."
+        ?>
+        </h2>
+    </div>
+    
+</body>
+</html>
